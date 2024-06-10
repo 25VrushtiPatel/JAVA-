f6c8c1582f0e2509faef48b2e1c0fcdd8899eafb
25VrushtiPatel <148217942+25VrushtiPatel@users.noreply.github.com>
2024-06-10 19:20:50 +0530

    Add files via upload

---
+++ UserDefine/src/userdefine/InvalidMarksException.java
@@ -0,0 +1,13 @@
+package userdefine;
+
+public class InvalidMarksException extends Exception {
+	
+	public InvalidMarksException() {
+		super();
+	}
+	
+	public InvalidMarksException(String msg) {
+		super(msg);
+	}
+
+}
