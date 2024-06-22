45d615c63a3bbc5ecde755e3b5fc4c4126b87f3c
25VrushtiPatel <148217942+25VrushtiPatel@users.noreply.github.com>
2024-06-22 20:09:44 +0530

    Add files via upload

---
+++ ThreadSynchronizationExample/src/synchronization/Bank.java
@@ -0,0 +1,7 @@
+package synchronization;
+public interface Bank {
+	int MINBAL=5000;
+	static final int DAILY_LIMIT=25000;
+	public abstract void deposit(int amt) throws DepositLimitExceedsException;
+	void withdraw(int amt)throws InsufficientBalanceException;
+}
