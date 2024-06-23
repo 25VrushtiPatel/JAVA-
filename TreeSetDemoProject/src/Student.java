4d2427d7df6d9d2ee06a194244a56f462a572bcd
25VrushtiPatel <148217942+25VrushtiPatel@users.noreply.github.com>
2024-06-23 17:20:00 +0530

    Add files via upload

---
+++ TreeSetDemoProject/src/Student.java
@@ -0,0 +1,21 @@
+public class Student implements Comparable<Student> {
+    private int id;
+    private String name;
+    private int marks;
+
+    public Student(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return Integer.compare(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", marks=" + marks + "]";
+    }
+}
